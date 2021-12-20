87d36b79ed8ddc0ef671ea6ceee987ca214aee2b
xgervilla <84778866+xgervilla@users.noreply.github.com>
2021-12-21 00:11:17 +0100

    Create problema_v1.py
    Muy poco eficiente, no sirve para el problema 2.

---
+++ Day 6/problema_v1.py
@@ -0,0 +1,25 @@
+def readOGfishes():
+	f = open('input.txt', 'r')
+	line = f.readline()
+	return [int(val) for val in line.split(',')]
+
+def computeNewDay(fishes):
+	countNewFishes = 0
+	for i in range(len(fishes)):
+		if fishes[i] == 0:
+			fishes[i] = 6
+			countNewFishes+=1
+		else:
+			fishes[i]-=1
+	for i in range(countNewFishes):
+		fishes.append(8)
+
+
+if __name__ == '__main__':
+	#leemos los peces iniciales (dias que faltan para que pongan huevos)
+	fishes = readOGfishes()
+	#calculamos dia a dia como evoluciona la poblacion de peces
+	for i in range(80):
+		computeNewDay(fishes)
+	#obtenemos el numero de peces tras los 80 dias
+	print(len(fishes))
